fa87419682ad94f6800134c8b83ee20c80cab6f6
Mehdi Hioul <mehdi@email.com>
2025-05-25 02:50:49 +0400

    ok

---
+++ .next/standalone/.next/server/pages/sitemap.xml.js
@@ -0,0 +1,51 @@
+(()=>{var e={};e.id=164,e.ids=[164,660],e.modules={1323:(e,t)=>{"use strict";Object.defineProperty(t,"l",{enumerable:!0,get:function(){return function e(t,r){return r in t?t[r]:"then"in t&&"function"==typeof t.then?t.then(t=>e(t,r)):"function"==typeof t&&"default"===r?t:void 0}}})},472:(e,t,r)=>{"use strict";r.r(t),r.d(t,{config:()=>S,default:()=>g,getServerSideProps:()=>h,getStaticPaths:()=>f,getStaticProps:()=>m,reportWebVitals:()=>P,routeModule:()=>q,unstable_getServerProps:()=>w,unstable_getServerSideProps:()=>_,unstable_getStaticParams:()=>x,unstable_getStaticPaths:()=>v,unstable_getStaticProps:()=>y});var a={};r.r(a),r.d(a,{default:()=>p,getServerSideProps:()=>d});var i=r(7093),n=r(5244),o=r(1323),s=r(2899),l=r.n(s),c=r(7352);let u="https://kickoutcancer.org",d=async({res:e})=>{let t=`<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>
+       <loc>${u}</loc>
+       <lastmod>${new Date().toISOString()}</lastmod>
+       <changefreq>daily</changefreq>
+       <priority>1.0</priority>
+     </url>
+     <url>
+       <loc>${u}/qui-sommes-nous</loc>
+       <lastmod>${new Date().toISOString()}</lastmod>
+       <changefreq>weekly</changefreq>
+       <priority>0.8</priority>
+     </url>
+     <url>
+       <loc>${u}/event-2025</loc>
+       <lastmod>${new Date().toISOString()}</lastmod>
+       <changefreq>daily</changefreq>
+       <priority>0.9</priority>
+     </url>
+     <url>
+       <loc>${u}/inscription</loc>
+       <lastmod>${new Date().toISOString()}</lastmod>
+       <changefreq>daily</changefreq>
+       <priority>0.9</priority>
+     </url>
+     <url>
+       <loc>${u}/partenaires</loc>
+       <lastmod>${new Date().toISOString()}</lastmod>
+       <changefreq>weekly</changefreq>
+       <priority>0.8</priority>
+     </url>
+     <url>
+       <loc>${u}/impact</loc>
+       <lastmod>${new Date().toISOString()}</lastmod>
+       <changefreq>daily</changefreq>
+       <priority>0.8</priority>
+     </url>
+     <url>
+       <loc>${u}/contact</loc>
+       <lastmod>${new Date().toISOString()}</lastmod>
+       <changefreq>monthly</changefreq>
+       <priority>0.7</priority>
+     </url>
+   </urlset>
+ `;return e.setHeader("Content-Type","text/xml"),e.write(t),e.end(),{props:{}}},p=function(){return null},g=(0,o.l)(a,"default"),m=(0,o.l)(a,"getStaticProps"),f=(0,o.l)(a,"getStaticPaths"),h=(0,o.l)(a,"getServerSideProps"),S=(0,o.l)(a,"config"),P=(0,o.l)(a,"reportWebVitals"),y=(0,o.l)(a,"unstable_getStaticProps"),v=(0,o.l)(a,"unstable_getStaticPaths"),x=(0,o.l)(a,"unstable_getStaticParams"),w=(0,o.l)(a,"unstable_getServerProps"),_=(0,o.l)(a,"unstable_getServerSideProps"),q=new i.PagesRouteModule({definition:{kind:n.x.PAGES,page:"/sitemap.xml",pathname:"/sitemap.xml",bundlePath:"",filename:""},components:{App:c.default,Document:l()},userland:a})},7352:(e,t,r)=>{"use strict";r.r(t),r.d(t,{default:()=>l});var a=r(997);let i=require("next/head");var n=r.n(i),o=r(4298),s=r.n(o);function l({Component:e,pageProps:t}){return(0,a.jsxs)(a.Fragment,{children:[(0,a.jsxs)(n(),{children:[a.jsx("meta",{charSet:"utf-8"}),a.jsx("meta",{name:"viewport",content:"width=device-width, initial-scale=1"}),a.jsx("link",{rel:"icon",href:"/images/favicon.svg"}),a.jsx("script",{type:"application/ld+json",dangerouslySetInnerHTML:{__html:JSON.stringify({"@context":"https://schema.org","@type":"Event",name:"Tournoi de Gala Kick Out Cancer 2025",startDate:"2025-09-13T09:00:00+02:00",endDate:"2025-09-13T18:00:00+02:00",location:{"@type":"Place",name:"Stade de CentraleSup\xe9lec",address:{"@type":"PostalAddress",streetAddress:"3 rue Joliot Curie",addressLocality:"Gif-sur-Yvette",postalCode:"91190",addressCountry:"FR"}},organizer:{"@type":"Organization",name:"Kick Out Cancer",url:"https://kickoutcancer.org"},description:"Tournoi de football caritatif pour soutenir la recherche contre le cancer \xe0 l'IHU PRISM.",image:"https://kickoutcancer.org/images/save-the-date-affiche.jpg"})}})]}),a.jsx(s(),{src:`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`,strategy:"afterInteractive"}),a.jsx(s(),{id:"google-analytics",strategy:"afterInteractive",children:`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}),a.jsx(e,{...t})]})}r(6764)},6764:()=>{},5244:(e,t)=>{"use strict";var r;Object.defineProperty(t,"x",{enumerable:!0,get:function(){return r}}),function(e){e.PAGES="PAGES",e.PAGES_API="PAGES_API",e.APP_PAGE="APP_PAGE",e.APP_ROUTE="APP_ROUTE"}(r||(r={}))},2785:e=>{"use strict";e.exports=require("next/dist/compiled/next-server/pages.runtime.prod.js")},6689:e=>{"use strict";e.exports=require("react")},6405:e=>{"use strict";e.exports=require("react-dom")},997:e=>{"use strict";e.exports=require("react/jsx-runtime")},5315:e=>{"use strict";e.exports=require("path")}};var t=require("../webpack-runtime.js");t.C(e);var r=e=>t(t.s=e),a=t.X(0,[899,298],()=>r(472));module.exports=a})();
