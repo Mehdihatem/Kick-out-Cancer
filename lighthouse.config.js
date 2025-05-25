ac5bace8b8e3a08e72d673114b1c956d049e0bcc
Mehdi Hioul <mehdi@email.com>
2025-05-26 00:42:53 +0400

    remodif ?

--- lighthouse.config.js
+++ lighthouse.config.js
@@ -2,7 +2,11 @@ module.exports = {
   ci: {
     collect: {
       startServerCommand: 'npm run start',
-      url: ['http://localhost:3000'],
+      url: [
+        'http://localhost:3000',
+        'http://localhost:3000/qui-sommes-nous',
+        'http://localhost:3000/event-2025'
+      ],
       numberOfRuns: 3,
     },
     assert: {
