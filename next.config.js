a3501f7d833626bfe887f49f6f6d847467e8b25f
Mehdi Hioul <mehdi@email.com>
2025-05-25 19:30:23 +0400

    Plus d'erreur ?

--- next.config.js
+++ next.config.js
@@ -35,18 +35,6 @@ const nextConfig = {
   },
   basePath: process.env.NODE_ENV === 'production' ? '/kick-out-cancer' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/kick-out-cancer/' : '',
-  async generateStaticParams() {
-    return {
-      '/robots.txt': {
-        page: '/robots.txt',
-        query: {},
-      },
-      '/sitemap.xml': {
-        page: '/sitemap.xml',
-        query: {},
-      },
-    }
-  },
 }
 
 module.exports = nextConfig 
