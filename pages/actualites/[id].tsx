d4ec92cde5f38f95edc01d54fbd2a1ce29088e23
Mehdi Hioul <mehdi@email.com>
2025-06-02 12:39:36 +0400

    mODITF DE ID

--- pages/actualites/[id].tsx
+++ pages/actualites/[id].tsx
@@ -7,8 +7,8 @@ export default function ActualiteDetail() {
   return (
     <main className="min-h-screen flex items-center justify-center">
       <div>
-        <h1 className="text-3xl font-bold mb-4">Détail de l'actualité {id}</h1>
-        <p>Contenu à venir…</p>
+        <h1 className="text-3xl font-bold mb-4">{`Détail de l'actualité ${id}`}</h1>
+        <p>{`Contenu à venir…`}</p>
       </div>
     </main>
   )
